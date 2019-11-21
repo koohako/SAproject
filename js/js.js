60063df3e10b5b273d50c5c5a7d1d26bf037b271
koohako <arturguimaraesdefreitas@gmail.com>
2019-11-21 14:53:43 -0300

    jogabilidadade quase la

--- js/js.js
+++ js/js.js
@@ -1,18 +1,12 @@
 function esquerda(){
     var carta = document.getElementById("baixo")
       var fimanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend"
-      var linkImg = "COLOCAR O LINK DA IMAGEM DO PESOMAGEM QUE ESTA NA CARTA!!"
-      var textoNome = "Campista"
-      var textoFala = "tdxfchgvjhgfthydrfcgv nbkhgiyuftydfhcv bnkgiuyuftydfxgc gftcvbhgfcv buytgfv bnjhytgfcv bnhgfcv bnhgfc bnhgtrfedfcvbnhjuytfrvbnjuytf"
-      var paragrafo = document.getElementById("personagemNome")
-      var fala = document.getElementById("fala")
       carta.className = "form animated slideOutLeft"
+      resposta("esquerda",NovaCarta())
       setTimeout(function(){
-        paragrafo.innerHTML = textoNome
-        fala.innerHTML = textoFala
         carta.className = "form animated fadeInDown"
         var img = document.getElementById("personagem")
-        img.src = linkImg
+        //img.src = linkImg
         setTimeout(function(){
           carta.classList += fimanimacao
         }, 1000)
@@ -21,12 +15,12 @@ function esquerda(){
    function direita(){
       var carta = document.getElementById("baixo")
       var fimanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend"
-      var linkImg = "COLOCAR O LINK DA IMAGEM DO PESOMAGEM QUE ESTA NA CARTA!!"
       carta.className = "form animated slideOutRight"
+      resposta("direita",NovaCarta())
       setTimeout(function(){
         carta.className = "form animated fadeInDown"
         var img = document.getElementById("personagem")
-        img.src = linkImg
+        //img.src = linkImg
         setTimeout(function(){
           carta.classList += fimanimacao
         }, 1000)
@@ -34,22 +28,57 @@ function esquerda(){
    }
 
 
-   //
-var poderes = new Array()
+   
+var economia=50, recursos=50, coroa=50, maoDeObra=50
 var divFala = document.getElementById("fala")
 var divNome = document.getElementById("personagemNome")
 
-function cartas( fala, nome) {
-    var maxEventos = 1
-    var evento = Math.float(Math.random() * maxEventos + 1)
-    if(evento==0){
-        nome.innerHTML = "cleidomiro"
-        fala.innerHTML = "ai chef ja vou avisando que vai dar merda isso aqui em, mas vc pode escolher a merda que vai dar, quer merda na economia ou nos recursos?"
+function NovaCarta() {
+    var maxEventos = 3
+    var evento = Math.floor(Math.random() * maxEventos + 1)
+    if(evento==1){
+        //var linkImg = "COLOCAR O LINK DA IMAGEM DO PESOMAGEM QUE ESTA NA CARTA!!"
+        divNome.innerHTML = "cleidomiro"
+        divFala.innerHTML = "ai chef ja vou avisando que vai dar merda isso aqui em, mas vc pode escolher a merda que vai dar, quer merda na economia ou nos recursos?, esqueda para economia e direita para recursos"
         
     }
+    if(evento==2){
+      //var linkImg = "COLOCAR O LINK DA IMAGEM DO PESOMAGEM QUE ESTA NA CARTA!!"
+      divNome.innerHTML = "juliana"
+      divFala.innerHTML = "ai chef ja vou avisando que vai dar merda isso aqui em, mas vc pode escolher a merda que vai dar, quer merda na economia ou nos recursos?, esqueda para economia e direita para recursos"
+      
+  }
+    if(evento==3){
+    //var linkImg = "COLOCAR O LINK DA IMAGEM DO PESOMAGEM QUE ESTA NA CARTA!!"
+    divNome.innerHTML = "sebastian"
+    divFala.innerHTML = " esquerda aumenta economia diminui recursos"
+    
+}
+    return evento;
 
 }
 
+function resposta(respostaAux,evento){
+  if (evento==1){
+    if(respostaAux=="esqueda")
+      economia -= 20
+    if(respostaAux=="direita")
+      recursos -= 20
+  }
+  if (evento==2){
+    if(respostaAux=="esquerda")
+      economia -= 20
+    if(respostaAux=="direita")
+      recursos -= 20
+  }
+  if (evento==3){
+    if(respostaAux=="esquerda")
+      economia -= 20
+    if(respostaAux=="direita")
+      recursos -= 20
+  }
+  
+}
 
 
 
