9670a82a0b3b91d14af16d1783e3b5a4c180a8e2
artureller <xarturellerx@gmail.com>
2019-11-26 17:22:10 -0300

    ganbiarra

--- js/js.js
+++ js/js.js
@@ -1,6 +1,3 @@
-var imported = document.createElement('script');
-imported.src="sweetalert2.all.js";
-document.head.appendChild(imported);
 function esquerda(){
     var carta = document.getElementById("baixo")
       var fimanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend"
@@ -160,4 +157,40 @@ document.onkeyup(function(e){
     checkbox.checked = false
   }
 })
+function alertMenu(){
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, delete it!',
+    cancelButtonText: 'No, cancel!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.value) {
+      swalWithBootstrapButtons.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'Your imaginary file is safe :)',
+        'error'
+      )
+    }
+  })
+}
 
